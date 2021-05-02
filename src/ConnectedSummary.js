50e8b4b315babcf83696f8a088e5e971d547aa8f
Steve Kinney <hello@stevekinney.net>
2021-05-02 14:47:09 -0600

    Add selectors

--- src/ConnectedSummary.js
+++ src/ConnectedSummary.js
@@ -1,15 +1,32 @@
 import { connect } from 'react-redux';
+import { createSelector } from 'reselect';
 import { Summary } from './Summary';
 
+const getItems = (state) => state.items;
+const getTipPercentage = (state) => state.tipPercentage;
+
+const getSubtotal = createSelector([getItems], (items) => {
+  return items.reduce((sum, item) => sum + item.price * item.quantity, 0);
+});
+
+const getTipAmount = createSelector(
+  [getSubtotal, getTipPercentage],
+  (subtotal, tipPercentage) => {
+    return subtotal * (tipPercentage / 100);
+  }
+);
+
+const getTotal = createSelector(
+  [getSubtotal, getTipAmount],
+  (subtotal, tipAmount) => {
+    return subtotal + tipAmount;
+  }
+);
+
 const mapStateToProps = (state) => {
-  const subtotal = state.items.reduce(
-    (sum, item) => sum + item.price * item.quantity,
-    0
-  );
-
-  const tipAmount = subtotal * (state.tipPercentage / 100);
-
-  const total = subtotal + tipAmount;
+  const subtotal = getSubtotal(state);
+  const tipAmount = getTipAmount(state);
+  const total = getTotal(state);
 
   return {
     subtotal,
