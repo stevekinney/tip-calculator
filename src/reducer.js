2b3974bdca95d41c5c74c950f9b3a4192b88e368
Steve Kinney <hello@stevekinney.net>
2021-05-02 08:04:38 -0600

    Refactor reducer

--- src/reducer.js
+++ src/reducer.js
@@ -1,12 +1,14 @@
+import { combineReducers } from 'redux';
+
 let id = 1;
 
-export const initialState = {
-  items: [
-    { uuid: id++, name: 'Tofu Roast', price: 14, quantity: 1 },
-    { uuid: id++, name: 'Vegan Ham', price: 12, quantity: 1 }
-  ]
-};
+export const initialItems = [
+  { uuid: id++, name: 'Tofu Roast', price: 14, quantity: 1 },
+  { uuid: id++, name: 'Vegan Ham', price: 12, quantity: 1 }
+];
 
-export const reducer = (state = initialState, action) => {
+export const items = (state = initialItems, action) => {
   return state;
 };
+
+export const reducer = combineReducers({ items });
