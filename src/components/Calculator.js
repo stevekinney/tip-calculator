b4550b7824531c6a10e8eb62eca8a04f50cbcee5
Steve Kinney <hello@stevekinney.net>
2021-05-03 17:43:57 -0600

    Implement MenuItemsContainer

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,20 +1,15 @@
 import { Card } from '@twilio-paste/core';
+import { MenuItemsContainer } from '../containers/MenuItemsContainer';
 import { TipSelectContainer } from '../containers/TipSelectContainer';
 
-import { MenuItems } from './MenuItems';
 import { NewItemForm } from './NewItemForm';
 import { Summary } from './Summary';
-
-const items = [
-  { uuid: 1, name: 'Tofu Roast', price: 14, quantity: 1 },
-  { uuid: 2, name: 'Vegan Ham', price: 12, quantity: 1 }
-];
 
 const Calculator = () => {
   return (
     <Card>
       <NewItemForm />
-      <MenuItems items={items} />
+      <MenuItemsContainer />
       <TipSelectContainer />
       <Summary />
     </Card>
