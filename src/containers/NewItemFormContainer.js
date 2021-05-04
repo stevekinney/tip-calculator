450aadcb9363733cd9402faa97e441b7552fc83a
Steve Kinney <hello@stevekinney.net>
2021-05-03 18:00:25 -0600

    Bind the action creators

--- src/containers/NewItemFormContainer.js
+++ src/containers/NewItemFormContainer.js
@@ -2,10 +2,8 @@ import { connect } from 'react-redux';
 import { NewItemForm } from '../components/NewItemForm';
 import { addNewItem } from '../store/items/actions';
 
-const mapDispatchToProps = (dispatch) => {
-  return {
-    onSubmit: (name, price) => dispatch(addNewItem(name, price))
-  };
+const mapDispatchToProps = {
+  onSubmit: addNewItem
 };
 
 export const NewItemFormContainer = connect(
