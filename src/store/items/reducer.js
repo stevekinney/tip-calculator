8a7b2aeed48af497c181468c521046cb2b7d775c
Steve Kinney <hello@stevekinney.net>
2021-05-03 20:18:12 -0600

    Use Immer for the entire reducer

--- src/store/items/reducer.js
+++ src/store/items/reducer.js
@@ -13,18 +13,16 @@ export const initialItems = [
   { uuid: id++, name: 'Vegan Ham', price: 12, quantity: 1 }
 ];
 
-export const reducer = (state = initialItems, action) => {
+export const reducer = produce((state = initialItems, action) => {
   if (action.type === ITEM_ADDED) {
-    return produce(state, (draftState) => {
-      const item = {
-        uuid: id++,
-        quantity: 1,
-        name: action.payload.name,
-        price: parseInt(action.payload.price, 10)
-      };
+    const item = {
+      uuid: id++,
+      quantity: 1,
+      name: action.payload.name,
+      price: parseInt(action.payload.price, 10)
+    };
 
-      draftState.push(item);
-    });
+    state.push(item);
   }
 
   if (action.type === ITEM_REMOVED) {
@@ -32,20 +30,14 @@ export const reducer = (state = initialItems, action) => {
   }
 
   if (action.type === ITEM_PRICE_UPDATED) {
-    return produce(state, (draftState) => {
-      const item = draftState.find((item) => item.uuid === action.payload.uuid);
-      item.price = parseInt(action.payload.price, 10);
-    });
+    const item = state.find((item) => item.uuid === action.payload.uuid);
+    item.price = parseInt(action.payload.price, 10);
   }
 
   if (action.type === ITEM_QUANTITY_UPDATED) {
-    return produce(state, (draftState) => {
-      const item = draftState.find((item) => item.uuid === action.payload.uuid);
-      item.quantity = parseInt(action.payload.quantity, 10);
-    });
+    const item = state.find((item) => item.uuid === action.payload.uuid);
+    item.quantity = parseInt(action.payload.quantity, 10);
   }
-
-  return state;
-};
+}, initialItems);
 
 export default reducer;
