8df6e0b986f8d90f95dba32f4d55f4c21d983f8d
Steve Kinney <hello@stevekinney.net>
2021-05-03 18:31:46 -0600

    Add logic for removing an item

--- src/store/items/actions.js
+++ src/store/items/actions.js
@@ -1,4 +1,5 @@
 export const ITEM_ADDED = 'ITEM_ADDED';
+export const ITEM_REMOVED = 'ITEM_REMOVED';
 
 export const addNewItem = (name, price) => {
   return {
@@ -6,6 +7,15 @@ export const addNewItem = (name, price) => {
     payload: {
       name,
       price
+    }
+  };
+};
+
+export const removeItem = (uuid) => {
+  return {
+    type: ITEM_REMOVED,
+    payload: {
+      uuid
     }
   };
 };
