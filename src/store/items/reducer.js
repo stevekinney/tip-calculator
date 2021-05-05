718ee4b85f41afcb3cc0c372297e94926b809e2a
Steve Kinney <hello@stevekinney.net>
2021-05-05 15:41:44 -0500

    With Immer

--- src/store/items/reducer.js
+++ src/store/items/reducer.js
@@ -13,12 +13,10 @@ export const initialItems = [
   { uuid: id++, name: 'Vegan Ham Sandwich', price: 12, quantity: 1 }
 ];
 
-export const reducer = (state = initialItems, action) => {
+export const reducer = produce((state = initialItems, action) => {
   if (action.type === ITEM_ADDED) {
-    produce(state, (draftState) => {
-      const item = { uuid: id++, quantity: 1, ...action.payload };
-      draftState.push(item);
-    });
+    const item = { uuid: id++, quantity: 1, ...action.payload };
+    state.push(item);
   }
 
   if (action.type === ITEM_REMOVED) {
@@ -26,22 +24,14 @@ export const reducer = (state = initialItems, action) => {
   }
 
   if (action.type === ITEM_PRICE_UPDATED) {
-    return produce(state, (draftState) => {
-      const item = draftState.find((item) => item.uuid === action.payload.uuid);
-      item.price = parseInt(action.payload.uuid, 10);
-    });
+    const item = state.find((item) => item.uuid === action.payload.uuid);
+    item.price = parseInt(action.payload.price, 10);
   }
 
   if (action.type === ITEM_QUANTITY_UPDATED) {
-    return state.map((item) => {
-      if (item.uuid === action.payload.uuid) {
-        return { ...item, quantity: action.payload.quantity };
-      }
-      return item;
-    });
+    const item = state.find((item) => item.uuid === action.payload.uuid);
+    item.quantity = parseInt(action.payload.quantity, 10);
   }
-
-  return state;
-};
+}, initialItems);
 
 export default reducer;
