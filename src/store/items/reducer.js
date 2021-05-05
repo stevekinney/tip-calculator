c596baf89cdb72ae637b39102662a6e53a65d636
Steve Kinney <hello@stevekinney.net>
2021-05-05 15:33:16 -0500

    Ready for immer exercise

--- src/store/items/reducer.js
+++ src/store/items/reducer.js
@@ -1,3 +1,4 @@
+import produce from 'immer';
 import {
   ITEM_ADDED,
   ITEM_PRICE_UPDATED,
@@ -14,8 +15,10 @@ export const initialItems = [
 
 export const reducer = (state = initialItems, action) => {
   if (action.type === ITEM_ADDED) {
-    const item = { uuid: id++, quantity: 1, ...action.payload };
-    return [...state, item];
+    produce(state, (draftState) => {
+      const item = { uuid: id++, quantity: 1, ...action.payload };
+      draftState.push(item);
+    });
   }
 
   if (action.type === ITEM_REMOVED) {
@@ -23,11 +26,9 @@ export const reducer = (state = initialItems, action) => {
   }
 
   if (action.type === ITEM_PRICE_UPDATED) {
-    return state.map((item) => {
-      if (item.uuid === action.payload.uuid) {
-        return { ...item, price: action.payload.price };
-      }
-      return item;
+    return produce(state, (draftState) => {
+      const item = draftState.find((item) => item.uuid === action.payload.uuid);
+      item.price = parseInt(action.payload.uuid, 10);
     });
   }
 
